90d9889ce5e2092603bf946a245b252793d6b05c
lubomyr1995 <lubomyr1995@gmail.com>
2022-05-17 17:03:17 +0300

    add lesson2.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,70 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './component/users/users.component';
+import {UseerComponent} from './component/useer/useer.component';
+import {UseerDetailsComponent} from './component/useer-details/useer-details.component';
+import {PostsComponent} from './component/posts/posts.component';
+import {PostComponent} from './component/post/post.component';
+import {PostDetailsComponent} from './component/post-details/post-details.component';
+import {CommentsComponent} from './component/comments/comments.component';
+import {CommentComponent} from './component/comment/comment.component';
+import {CommentDetailsComponent} from './component/comment-details/comment-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {MainLayoutComponent} from './layout/main-layout/main-layout.component';
+import {CanNotReadComponent} from './component/can-not-read/can-not-read.component';
+import {CommentsResolverService} from "./services";
+import {PostCheckerService} from "./services";
+import {DeactivateService} from "./services";
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {
+        path: 'users', component: UsersComponent, children: [
+          {path: 'details/:id', component: UseerDetailsComponent, canDeactivate: [DeactivateService]}
+        ]
+      },
+      {
+        path: 'posts', component: PostsComponent, children: [
+          {path: 'details/:id', component: PostDetailsComponent, canActivate: [PostCheckerService]}
+        ]
+      },
+      {
+        path: 'comments', component: CommentsComponent, resolve: {allComments: CommentsResolverService}, children: [
+          {path: 'details/:id', component: CommentDetailsComponent}
+        ]
+      }
+    ]
+  },
+  {path: '**', component: CanNotReadComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UseerComponent,
+    UseerDetailsComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+    CommentsComponent,
+    CommentComponent,
+    CommentDetailsComponent,
+    MainLayoutComponent,
+    CanNotReadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/services/index.ts
@@ -0,0 +1,6 @@
+export * from './user.service';
+export * from './post.service';
+export * from './comment.service';
+export * from './deactivate.service';
+export * from './post-checker.service';
+export * from './comments-resolver.service';
